0c1926ec1961fbcd7d589075d8e973100fb3ee62
Meet Guleria <meetguleria94@gmail.com>
2023-08-19 12:17:14 -0700

    vehicles-tests

--- backend/src/vehicles/vehicles.controller.spec.ts
+++ backend/src/vehicles/vehicles.controller.spec.ts
@@ -1,18 +1,67 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VehiclesController } from './vehicles.controller';
+import { VehiclesService } from './vehicles.service';
+
+const mockVehicle = {
+    model: 'Tesla',
+    make: 'Model S',
+    year: 2020,
+    locationId: 1,
+    price: 50000,
+    status: 'available',
+};
+
+const mockVehicleList = [mockVehicle];
+
+const mockAddResult = { insertId: 1 };
+const mockUpdateResult = { affectedRows: 1 };
+const mockDeleteResult = { affectedRows: 1 };
 
 describe('VehiclesController', () => {
-  let controller: VehiclesController;
+    let controller: VehiclesController;
+    let service: VehiclesService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [VehiclesController],
-    }).compile();
+    beforeEach(async () => {
+        service = {
+            getAllVehicles: jest.fn().mockResolvedValue(mockVehicleList),
+            addVehicle: jest.fn().mockResolvedValue(mockAddResult),
+            updateVehicle: jest.fn().mockResolvedValue(mockUpdateResult),
+            deleteVehicle: jest.fn().mockResolvedValue(mockDeleteResult),
+        } as any;
 
-    controller = module.get<VehiclesController>(VehiclesController);
-  });
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [VehiclesController],
+            providers: [{ provide: VehiclesService, useValue: service }],
+        }).compile();
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+        controller = module.get<VehiclesController>(VehiclesController);
+    });
+
+    it('should be defined', () => {
+        expect(controller).toBeDefined();
+    });
+
+    describe('getAllVehicles', () => {
+        it('should get all vehicles', async () => {
+            expect(await controller.getAllVehicles()).toEqual(mockVehicleList);
+        });
+    });
+
+    describe('addVehicle', () => {
+        it('should add a vehicle', async () => {
+            expect(await controller.addVehicle(mockVehicle)).toEqual(mockAddResult);
+        });
+    });
+
+    describe('updateVehicle', () => {
+        it('should update a vehicle', async () => {
+            expect(await controller.updateVehicle(1, mockVehicle)).toEqual(mockUpdateResult);
+        });
+    });
+
+    describe('deleteVehicle', () => {
+        it('should delete a vehicle', async () => {
+            expect(await controller.deleteVehicle(1)).toEqual(mockDeleteResult);
+        });
+    });
 });
--- backend/src/vehicles/vehicles.service.spec.ts
+++ backend/src/vehicles/vehicles.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VehiclesService } from './vehicles.service';
+import { DatabaseService } from '../database/database.service';
 
 describe('VehiclesService', () => {
     let service: VehiclesService;
+    let mockQuery: jest.Mock;
 
     beforeEach(async () => {
+        mockQuery = jest.fn();
+
         const module: TestingModule = await Test.createTestingModule({
-            providers: [VehiclesService],
+            providers: [
+                VehiclesService,
+                { provide: DatabaseService, useValue: { getConnection: () => ({ query: mockQuery }) } },
+            ],
         }).compile();
 
         service = module.get<VehiclesService>(VehiclesService);
@@ -15,4 +22,63 @@ describe('VehiclesService', () => {
     it('should be defined', () => {
         expect(service).toBeDefined();
     });
+
+    it('should get all vehicles', async () => {
+        const expectedResult = [{ 
+            VehicleID: 1, 
+            model: 'Tesla', 
+            make: 'Model S', 
+            year: 2020,
+            locationId: 1,
+            price: 50000,
+            status: 'available'
+        }];
+        mockQuery.mockReturnValueOnce([expectedResult]);
+
+        const result = await service.getAllVehicles();
+        expect(result).toEqual(expectedResult);
+    });
+
+    it('should add a vehicle', async () => {
+        const vehicle = { 
+            model: 'Tesla', 
+            make: 'Model S', 
+            year: 2020,
+            locationId: 1,
+            price: 50000,
+            status: 'available',
+        };
+        const expectedResult = [{ insertId: 1 }];
+
+        mockQuery.mockResolvedValueOnce([expectedResult]);
+
+        const result = await service.addVehicle(vehicle);
+        expect(result).toEqual(expectedResult);
+    });
+
+    it('should update a vehicle', async () => {
+        const vehicle = {
+            model: 'Toyota',
+            make: 'Prius',
+            year: 2018,
+            locationId: 1,
+            price: 25000,
+            status: 'unavailable'
+        };
+        const expectedResult = { affectedRows: 1 };
+
+        mockQuery.mockResolvedValueOnce([expectedResult]);
+
+        const result = await service.updateVehicle(1, vehicle);
+        expect(result).toEqual(expectedResult);
+    });
+
+    it('should delete a vehicle', async () => {
+        const expectedResult = { affectedRows: 1};
+
+        mockQuery.mockResolvedValueOnce([expectedResult]);
+
+        const result = await service.deleteVehicle(1);
+        expect(result).toEqual(expectedResult);
+    })
 });
